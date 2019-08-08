ce35448b9cccdd58ebd2b91cd8bbd90b45d6465f
DanBehrman <dan.behrman@wework.com>
2019-08-08 14:29:14 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
 
+    def initialize(name)
+        @name = name 
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        p "Your balance is $#{self.balance}."
+    end
+
+    def valid? 
+        if self.balance > 0 && self.status == "open"
+            return true
+        else
+            return false 
+        end 
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
+require 'pry'
 class Transfer
-  # your code here
+    attr_accessor :sender, :receiver, :status , :amount
+
+    @@all_transfers =[] 
+
+  def self.all_transfers
+    @@all_transfers
+  end
+
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid? 
+    if self.sender.valid? && self.receiver.valid? 
+      return true
+    else 
+      return false
+    end 
+  end 
+
+  def past_transaction
+    @@all_transfers.select do |transfer|
+      transfer.sender.name == self.sender.name && transfer.receiver.name == self.receiver.name
+    end 
+  end 
+
+  def execute_transaction
+    #binding.pry
+    if self.sender.balance < amount && !self.valid?
+      return "Transaction rejected. Please check your account balance."
+    end 
+    if self.valid? && self.past_transaction.length == 0
+      self.sender.balance -= amount
+      self.receiver.balance += amount
+      self.status = "complete"
+      @@all_transfers << self
+    end 
+  end
+
+  binding.pry
+
+
+
+
+
+
 end
