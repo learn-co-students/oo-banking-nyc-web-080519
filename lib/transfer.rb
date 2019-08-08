c4c96ee8993ec8cf41345b56702ae4d2b8180d9b
DanBehrman <dan.behrman@wework.com>
2019-08-08 16:50:26 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,30 +23,23 @@ class Transfer
     end 
   end 
 
-  def past_transaction
-    @@all_transfers.select do |transfer|
-      transfer.sender.name == self.sender.name && transfer.receiver.name == self.receiver.name
-    end 
-  end 
-
   def execute_transaction
     #binding.pry
-    if self.sender.balance < amount && !self.valid?
-      return "Transaction rejected. Please check your account balance."
-    end 
-    if self.valid? && self.past_transaction.length == 0
+    if self.valid? && self.status != "complete" && self.sender.balance > amount
       self.sender.balance -= amount
       self.receiver.balance += amount
       self.status = "complete"
-      @@all_transfers << self
+    else
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end 
   end
 
-  binding.pry
-
-
-
-
-
-
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
 end
